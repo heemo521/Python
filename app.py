3a03666cc91aecef603cb41a2343b855d1d484ee
Heemo Yang <heemo521@gmail.com>
2022-11-01 16:23:38 -0700

    main

--- app.py
+++
@@ -1,9 +0,0 @@
-age = 20
-price = 19.95
-first_name = "Kevin"
-is_Online = True
-print(age)
-print(price)
-print(first_name)
-print(is_Online)
-
---
+++ main.py
@@ -0,0 +1,14 @@
+
+price = 19.95
+first_name = "Kevin"
+is_Online = True
+print(age)
+print(price)
+print(first_name)
+print(is_Online)
+
+name = input('What is your name? ')
+print('Hello ' + name)
+
+birth_year = input("Enter your birth year: ")
+age = 2022 - birth_year
