ab86cbd896edaa8b4bc113d43c7fff742f7564a7
Heemo Yang <heemo521@gmail.com>
2022-11-01 16:38:54 -0700

    String methods

--- main.py
+++ main.py
@@ -10,16 +10,24 @@
 # name = input('What is your name? ')
 # print('Hello ' + name)
 
-birth_year = input("Enter your birth year: ")
-age = 2022 - int(birth_year)
-print(age)
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
 
-int()
-float()
-bool()
-str()
+# int()
+# float()
+# bool()
+# str()
 
-firstNum = float(input('First Number: '))
-secondNum = float(input('Second Number: '))
-sum = firstNum + secondNum
-print("Sum: " + str(sum))
+# firstNum = float(input('First Number: '))
+# secondNum = float(input('Second Number: '))
+# sum = firstNum + secondNum
+# print("Sum: " + str(sum))
+
+course = "Python for Beginners"
+print(course)
+print('Python' in course)
+course = course.upper()
+print(course)
+print(course.find('FOR', 8))
+print(course.replace('for'.upper(), '4'))
