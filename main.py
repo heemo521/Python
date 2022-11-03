220268d9020bee69e65eb7fcf312b2f44daaca29
Heemo Yang <79474397+heemo521@users.noreply.github.com>
2022-11-02 22:03:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 
-# price = 19.95
-# first_name = "Kevin"
-# is_Online = True
+price = 19.95
+first_name = "Kevin"
+is_Online = True
 
-# print(price)
-# print(first_name)
-# print(is_Online)
+print(price)
+print(first_name)
+print(is_Online)
 
-# name = input('What is your name? ')
-# print('Hello ' + name)
+name = input('What is your name? ')
+print('Hello ' + name)
 
-# birth_year = input("Enter your birth year: ")
-# age = 2022 - int(birth_year)
-# print(age)
+birth_year = input("Enter your birth year: ")
+age = 2022 - int(birth_year)
+print(age)
 
-# int()
-# float()
-# bool()
-# str()
+int()
+float()
+bool()
+str()
 
-# firstNum = float(input('First Number: '))
-# secondNum = float(input('Second Number: '))
-# sum = firstNum + secondNum
-# print("Sum: " + str(sum))
+firstNum = float(input('First Number: '))
+secondNum = float(input('Second Number: '))
+sum = firstNum + secondNum
+print("Sum: " + str(sum))
 
 course = "Python for Beginners"
 print(course)
